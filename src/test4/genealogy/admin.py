c7a88135241fe7392460e5c93596a80bd185876c
Chris Dew <cmsdew@gmail.com>
2010-09-26 22:33:39 +0100

    many changes

--- src/test4/genealogy/admin.py
+++ src/test4/genealogy/admin.py
@@ -12,7 +12,51 @@ class UserProfileInline(admin.StackedInline):
 class UserProfileAdmin(UserAdmin):
     inlines = [UserProfileInline]
  
+class ManManInline(admin.StackedInline): 
+    fk_name = 'father'
+    extra=1
+    model = models.Man
+    
+class WomanManInline(admin.StackedInline): 
+    fk_name = 'father'
+    extra=1
+    model = models.Woman
+ 
+class BaptismInline(admin.StackedInline):
+    extra=1
+    model = models.Baptism
+
+class OccupationInline(admin.StackedInline):
+    extra=1
+    model = models.Occupation
+
+class CensusInline(admin.StackedInline):
+    extra=1
+    model = models.Census
+
+class MarriageManInline(admin.StackedInline):
+    fk_name = 'husband'
+    extra=1
+    model = models.Marriage 
+
+class MarriageWomanInline(admin.StackedInline):
+    fk_name = 'wife'
+    extra=1
+    model = models.Marriage 
+ 
+class ManAdmin(admin.ModelAdmin):
+    inlines = [BaptismInline, MarriageManInline, ManManInline, WomanManInline, OccupationInline, CensusInline] 
+
+class WomanAdmin(admin.ModelAdmin):
+    inlines = [BaptismInline, MarriageWomanInline, OccupationInline, CensusInline] 
+
+ 
 admin.site.register(User, UserProfileAdmin)
 
-admin.site.register(models.Person)
+admin.site.register(models.Person) #, PersonAdmin)
+admin.site.register(models.Man, ManAdmin)
+admin.site.register(models.Woman, WomanAdmin)
 admin.site.register(models.Marriage)
+admin.site.register(models.Baptism)
+admin.site.register(models.Occupation)
+admin.site.register(models.Census)
--- src/test4/genealogy/models.py
+++ src/test4/genealogy/models.py
@@ -2,23 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Person(models.Model):
-    GENDER_CHOICES = (
-        (u'M', u'Male'),
-        (u'F', u'Female'),
-    )
+    #GENDER_CHOICES = (
+    #    (u'M', u'Male'),
+    #    (u'F', u'Female'),
+    #)
     
     first_name = models.CharField(max_length=31)
     second_name = models.CharField(max_length=31, blank=True)
     third_name = models.CharField(max_length=31, blank=True)
-    surname_at_birth = models.CharField(max_length=31)
-    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    surname_at_birth = models.CharField(max_length=31, blank=True)
+    #gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
     date_of_birth = models.DateField(blank=True, null=True)
+    place_of_birth = models.CharField(max_length=255, blank=True)
     date_of_death = models.DateField(blank=True, null=True)
 
-    mother = models.ForeignKey("Person", related_name="mothered", blank=True, null=True)
-    father = models.ForeignKey("Person", related_name="fathered", blank=True, null=True)
+    mother = models.ForeignKey("Woman", blank=True, null=True)
+    father = models.ForeignKey("Man", blank=True, null=True)
+    father_occupation = models.CharField(max_length=31, blank=True)
     
     notes = models.TextField(blank=True)
+    
     
     def __unicode__(self):
         return self.first_name + " " + self.surname_at_birth # + " (" + str(self.date_of_birth) + ")"
@@ -26,10 +29,55 @@ class Person(models.Model):
     class Meta:
         verbose_name_plural = "people"
     
+class Man(Person):
+    def __unicode__(self):
+        marriages = Marriage.objects.filter(husband=self)
+        if len(marriages) > 0:
+            return self.first_name + " " + self.surname_at_birth + ", husband of " + marriages[0].wife.first_name + " " + marriages[0].wife.surname_at_birth   
+        return self.first_name + " " + self.surname_at_birth # + " (" + str(self.date_of_birth) + ")"
+
+
+    class Meta:
+        verbose_name_plural = "men"
+
+class Woman(Person):
+    def __unicode__(self):
+        marriages = Marriage.objects.filter(wife=self)
+        if len(marriages) > 0:
+            return self.first_name + " " + self.surname_at_birth + ", wife of " + marriages[0].husband.first_name + " " + marriages[0].husband.surname_at_birth   
+        return self.first_name + " " + self.surname_at_birth # + " (" + str(self.date_of_birth) + ")"
+
+    class Meta:
+        verbose_name_plural = "women"
+        
+    
+class Baptism(models.Model):
+    person = models.ForeignKey(Person)
+    date_of_baptism = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True)
+    
+class Occupation(models.Model):
+    person = models.ForeignKey(Person)
+    year = models.IntegerField(null=True, blank=True)
+    occupation = models.CharField(max_length=31, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    
+class Census(models.Model):
+    person = models.ForeignKey(Person)
+    year = models.IntegerField(null=True, blank=True)
+    subdistrict = models.CharField(max_length=255, blank=True)
+    ed = models.CharField(max_length=4, blank=True)
+    head_of_household = models.CharField(max_length=255, blank=True)
+    birthplace = models.CharField(max_length=255, blank=True)
+    living_with = models.CharField(max_length=255, blank=True)
+    
+    
 class Marriage(models.Model):
-    husband = models.ForeignKey(Person, related_name="husband_of")
-    wife = models.ForeignKey(Person, related_name="wife_of")
+    husband = models.ForeignKey(Man, related_name="husband_of")
+    wife = models.ForeignKey(Woman, related_name="wife_of")
     date_of_marriage = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True)    
+    witness_names = models.CharField(max_length=255, blank=True)    
     
     def __unicode__(self):
         return "marriage of " + self.husband.first_name + " " + self.husband.surname_at_birth + " to " + self.wife.first_name + " " + self.wife.surname_at_birth
